8aafad769d7c27cb8ad4d369895f5864c9da3902
Eric Nelson <nelseric@mail.gvsu.edu>
2011-03-02 10:59:55 -0500

    stuff

--- src/nelseric/Main.java
+++ src/nelseric/Main.java
@@ -1,12 +1,15 @@
 package nelseric;
 
 public class Main {
+	
 
-	/**
-	 * @param args
-	 */
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int i = 1; i <= 40; i++) {
+			Hive.coordinates(i);
+		}
 
 	}
 
